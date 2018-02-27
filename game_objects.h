23a6e31f0656cb27ee8df7c4b2f80fb40a34bfcd
Corbin Stark <starkcorbin@gmail.com>
2018-02-26 16:47:02 -0800

    Update game_objects.h

--- game_objects.h
+++ game_objects.h
@@ -6,9 +6,6 @@
 
 struct Unit {
 	inline void dispose() {
-		disposeTexture(image_1);
-		disposeTexture(image_2);
-		disposeTexture(projectile);
 		free(identifier);
 		free(name);
 	}
@@ -26,12 +23,6 @@ struct Unit {
 	int move_range;
 	int attack_range;
 	int cost;
-	//I doubt I'd ever need more than 255 players on one map at a time...
-	unsigned char owner;
-	//there are only 2 frames of animation, and only one animation. to keep simple.
-	Texture image_1;
-	Texture image_2;
-	Texture projectile;
 };
 
 struct Squad {
@@ -44,37 +35,7 @@ struct Squad {
 	int speed;
 };
 
-struct Building {
-	inline void dispose() {
-		free(identifier);
-		free(name);
-	}
-	char* identifier;
-	char* name;
-	int cost;
-	int construction_time;
-	int population_change;
-	int population_growth_change;
-	int happiness_change;
-	std::vector<int> units_to_enable;
-};
-
-struct Hub {
-	Texture tex;
-};
-
-struct Town {
-	Texture tex;
-	std::vector<Unit> garrison;
-	std::vector<Building> buildings;
-	int happiness;
-	int population;
-	int population_growth;
-};
-
 void load_game_object_types();
 
 global_var int NUM_UNIT_TYPES;
 global_var Unit* UNIT_TYPES;
-global_var int NUM_BUILDING_TYPES;
-global_var Building* BUILDING_TYPES;
